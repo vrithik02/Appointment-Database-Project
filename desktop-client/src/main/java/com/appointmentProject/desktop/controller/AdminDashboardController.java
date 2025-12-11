e143bbefddc3ce1c3995e09ea755620ce8269906
Vrithik Raj <vrithik02@gmail.com>
2025-12-11 00:05:20 -0600

    Initial commit on vrithik-ManufacturerUI

--- desktop-client/src/main/java/com/appointmentProject/desktop/controller/AdminDashboardController.java
+++ desktop-client/src/main/java/com/appointmentProject/desktop/controller/AdminDashboardController.java
@@ -50,13 +50,12 @@ public class AdminDashboardController {
     }
 
     @FXML
-    private void handleLogout() {
-        SceneNavigator.switchTo("/fxml/login.fxml");
-    }
-
-    @FXML
     private void handleMedications() {
         SceneNavigator.switchTo("/fxml/Medication_list.fxml");
     }
 
+    @FXML
+    private void handleLogout() {
+        SceneNavigator.switchTo("/fxml/login.fxml");
+    }
 }
